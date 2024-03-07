9cb905ade052b75bca5ae5083bfa2fbc78b34466
Holly <hz2682@tc.columbia.edu>
2024-03-07 10:22:45 -0800

    Add findDistinctCities() method
    This method is prepared for possible future use. I was thinking about showing all recommendation options. But I ended up quitting it after a discussion with Wang because we better hide how small our database is.

--- src/main/java/com/project275/travelplaner/repository/RecommendationRepository.java
+++ src/main/java/com/project275/travelplaner/repository/RecommendationRepository.java
@@ -2,7 +2,11 @@ package com.project275.travelplaner.repository;
 import java.util.List;
 import com.project275.travelplaner.entity.Recommendation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RecommendationRepository extends JpaRepository<Recommendation, Integer> {
     List<Recommendation> findByCity(String city);
+
+    @Query("SELECT DISTINCT city FROM Recommendation")
+    List<String> findDistinctCities();
 }
