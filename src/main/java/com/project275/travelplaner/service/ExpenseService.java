ea3164ebb0c0c0d07d67fd6677145d7e38bf4272
Holly <hz2682@tc.columbia.edu>
2024-03-07 11:08:08 -0800

    Allow edit and update expense
    (1) Don't initialize new Expense() because this creates a new instance every time we want to edit, and the modifications are not made on the previous one. (2) when updating expense, copy all the data collected from HTML to the specific expense. (expenseLog_o -> expense)

--- src/main/java/com/project275/travelplaner/service/ExpenseService.java
+++ src/main/java/com/project275/travelplaner/service/ExpenseService.java
@@ -23,35 +23,37 @@ public class ExpenseService {
     @Autowired
     private UserRepository userRepo;
 
-    public String editExpense(@PathVariable int tripId, @PathVariable int itineraryId, ModelMap model){
-//        Trip trip = tripRepo.findById(tripId).orElse(null);
+    public String gotoEditExpense(int tripId, int itineraryId, ModelMap model){
         Itinerary itinerary = itineraryRepo.findById(itineraryId).orElse(null);
-        Expense expenseLog = new Expense();
-        if (itinerary != null) {
-            expenseLog = itinerary.getExpenseLog();
-            if (expenseLog == null) {
-                expenseLog = new Expense();
-                expenseLog.setItinerary(itinerary);
-                itinerary.setExpenseLog(expenseLog);
-                expenseRepo.save(expenseLog);
-            }
+        Trip trip = tripRepo.findById(tripId).orElse(null);
+        if (itinerary == null) {
+            model.put("itineraries", trip.getItineraries());
+            model.put("tripId", tripId);
+            model.put("trip", trip);
+            model.put("budget", trip.getBudget());
+            return "ViewItineraries";
         }
         model.put("name", itinerary.getName());
-        model.put("expenseLog", expenseLog);
         model.put("itinerary", itinerary);
+        model.put("expenseLog", itinerary.getExpenseLog());
         return "AddExpense";
     }
 
-    public String addExpense(int tripId, int itineraryId, Expense expenseLog, ModelMap model, HttpSession session){
+    public String updateExpense(int tripId, int itineraryId, Expense expenseLog_o, ModelMap model, HttpSession session){
         if (session.getAttribute("user") == null) {
             model.put("neg", "Invalid Session");
             return "Home";
         }
-
         Trip trip = tripRepo.findById(tripId).orElse(null);
-        expenseRepo.save(expenseLog);
+        Itinerary itinerary = itineraryRepo.findById(itineraryId).orElse(null);
+        itinerary.setExpenseSum(expenseLog_o.getSum());
+        Expense expense = itinerary.getExpenseLog();
+        expense.copy(expenseLog_o);
+        expenseRepo.save(expense);
         model.put("pos", "Expenses are Logged Successfully");
         model.put("itineraries", trip.getItineraries());
+        model.put("trip", trip);
+        model.put("budget", trip.getBudget());
         return "ViewItineraries";
     }
 }
